ba7c360b0d7c63a8cf21e94c3df6deb1ab1dfed5
Prasanga Fernando <prasangafdz@outlook.com>
2023-03-22 17:42:27 +0530

    Update SearchPage.java

--- src/main/java/com/instagram/android/page/SearchPage.java
+++ src/main/java/com/instagram/android/page/SearchPage.java
@@ -1,30 +1,42 @@
 package com.instagram.android.page;
 
 import com.google.common.collect.ImmutableMap;
-import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
+
 
 public class SearchPage extends CommonPage{
 
 private final By txtSearchBar = By.id("com.instagram.android:id/action_bar_search_edit_text");
-private final By btnAccountsTab = By.xpath("//android.widget.FrameLayout[@content-desc=\"Accounts\"]");
-    // User acceount view //android.widget.LinearLayout[contains(@resource-id,'row_search_user_info_container')]
+private final By btnAccountsTab = By.xpath("//android.widget.FrameLayout[@content-desc='Accounts']");
+private final By lblUserNames = By.xpath("//android.widget.LinearLayout[contains(@resource-id,'row_search_user_info_container')]");
+
     public void searchByUsername(String username){
+//        public void unfollowUsers(){
+//            followingUsersSet.add("nadeeshani_de_silva");
+//            for (String username:followingUsersSet) {
+//                searchPage.searchByUsername(username);
+//                searchPage.selectAccountsFromTabs();
+//                searchPage.selectFirstRecordFromSearchResults();
+//            }
+//
+//        }
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(8));
         wait.until(ExpectedConditions.elementToBeClickable(txtSearchBar)).click();
         wait.until(ExpectedConditions.elementToBeClickable(txtSearchBar)).sendKeys(username);
         driver.executeScript("mobile:performEditorAction", ImmutableMap.of("action", "done")); //This is used to click the return key
     }
 
-    public void selectAccounts(){
+    public void selectFirstRecordFromSearchResults(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(8));
+        wait.until(ExpectedConditions.elementToBeClickable(lblUserNames)).click();
+    }
+
+    public void selectAccountsFromTabs(){ //from tab
         driver.findElement(btnAccountsTab).click();
     }
 
