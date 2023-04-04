b7271785b6b4b8ccf68f98b735feae54dd954149
Prasanga Fernando <prasangafdz@outlook.com>
2023-04-04 16:43:26 +0530

    Create ChatModule.java

---
+++ src/main/java/com/instagram/android/module/ChatModule.java
@@ -0,0 +1,19 @@
+package com.instagram.android.module;
+
+import com.instagram.android.page.MessagesPage;
+
+public class ChatModule {
+
+    private static final MessagesPage message = new MessagesPage();
+
+    public static void navigateToMessages(){
+        message.clickOnMessagesButton();
+    }
+
+    public static boolean isMessagesTopicDisplayingProperly(){
+        return message.isMessagesTopicDisplayingProperly(); //convert to boolean
+    }
+
+
+
+}
