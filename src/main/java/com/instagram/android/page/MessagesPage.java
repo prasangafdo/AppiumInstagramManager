f5e9eb1c69620defb297f61f5f05187277b74233
Prasanga Fernando <prasangafdz@outlook.com>
2023-04-04 17:19:17 +0530

    Update MessagesPage.java

--- src/main/java/com/instagram/android/page/MessagesPage.java
+++ src/main/java/com/instagram/android/page/MessagesPage.java
@@ -10,11 +10,11 @@ public class MessagesPage extends CommonPage{
 
     private final By btnMessages = By.id("com.instagram.android:id/action_bar_inbox_button");
     private final By lblMessagesHeader = By.id("com.instagram.android:id/header_text");
+    private final By btnChatThreadOfUnreadUsers= By.xpath("//android.widget.TextView[@resource-id='com.instagram.android:id/row_inbox_digest' and contains(@text,'Sent')]/parent::android.view.View");
 
     public void clickOnMessagesButton(){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(ExpectedConditions.elementToBeClickable(btnMessages)).click();
-//        driver.findElement(btnProfile).click();
     }
 
     public boolean isMessagesTopicDisplayingProperly(){
@@ -22,6 +22,12 @@ public class MessagesPage extends CommonPage{
         return wait.until(ExpectedConditions.visibilityOfElementLocated(lblMessagesHeader)).getText().equals("Messages");
 //        driver.findElement(btnProfile).click();
     }
+
+   /*
+    *   gasther web elements
+    *   loop and navigate to other users page
+    *   do the unfollowing
+    */
 
 
 
